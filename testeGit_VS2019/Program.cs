45f83287a16705f8675186311072f21daa544e0d
Willem L. Ricardo <wlricardo@hotmail.com>
2019-04-03 23:49:30 -0300

    Programa inicial

---
+++ testeGit_VS2019/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testeGit_VS2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
